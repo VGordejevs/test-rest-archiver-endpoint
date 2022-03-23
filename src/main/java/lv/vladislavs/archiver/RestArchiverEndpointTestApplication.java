982adf0736a301cc6143769d0a42642f52c4954f
Vladislavs Gordejevs <vladislavs.gordejevs@gmail.com>
2022-03-23 23:50:52 +0200

    Initial commit for Spring Boot.

---
+++ src/main/java/lv/vladislavs/archiver/RestArchiverEndpointTestApplication.java
@@ -0,0 +1,13 @@
+package lv.vladislavs.archiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestArchiverEndpointTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestArchiverEndpointTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/vladislavs/archiver/RestArchiverEndpointTestApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.vladislavs.archiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestArchiverEndpointTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
