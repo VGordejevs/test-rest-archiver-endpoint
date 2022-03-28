e446a576ba0d1915cf92553a23bf86f30c96d534
Vladislavs Gordejevs <vladislavs.gordejevs@mappost.eu>
2022-03-28 23:46:25 +0300

    Implemented request logger using filter.

---
+++ src/main/java/lv/vladislavs/archiver/filter/RequestLogFilter.java
@@ -0,0 +1,37 @@
+package lv.vladislavs.archiver.filter;
+
+import lv.vladislavs.archiver.model.database.RequestLog;
+import lv.vladislavs.archiver.repository.RequestLogRepository;
+import lv.vladislavs.archiver.util.HttpReqRespUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+@Component
+public class RequestLogFilter implements Filter {
+    @Autowired
+    RequestLogRepository requestLogRepository;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        String ipAddress = HttpReqRespUtils.getIpAddress(httpServletRequest);
+        String servletPath = httpServletRequest.getServletPath();
+
+        requestLogRepository.save(RequestLog.builder()
+                .ipAddress(ipAddress)
+                .servletPath(servletPath)
+                .requestTime(OffsetDateTime.now(ZoneOffset.UTC))
+                .build());
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
---
+++ src/main/java/lv/vladislavs/archiver/util/HttpReqRespUtils.java
@@ -0,0 +1,35 @@
+package lv.vladislavs.archiver.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class HttpReqRespUtils {
+    private static final String[] IP_HEADER_CANDIDATES = {
+        "X-Forwarded-For",
+        "Proxy-Client-IP",
+        "WL-Proxy-Client-IP",
+        "HTTP_X_FORWARDED_FOR",
+        "HTTP_X_FORWARDED",
+        "HTTP_X_CLUSTER_CLIENT_IP",
+        "HTTP_CLIENT_IP",
+        "HTTP_FORWARDED_FOR",
+        "HTTP_FORWARDED",
+        "HTTP_VIA",
+        "REMOTE_ADDR"
+    };
+
+    /**
+     * Copied from https://stackoverflow.com/a/55266907/8740600
+     */
+    public static String getIpAddress(HttpServletRequest httpServletRequest) {
+        for (String header: IP_HEADER_CANDIDATES) {
+            String potentialIp = httpServletRequest.getHeader(header);
+            if (potentialIp != null
+                    && !potentialIp.isEmpty()
+                    && !"unknown".equalsIgnoreCase(potentialIp)) {
+                return potentialIp.split(",")[0];
+            }
+        }
+
+        return httpServletRequest.getRemoteAddr();
+    }
+}
