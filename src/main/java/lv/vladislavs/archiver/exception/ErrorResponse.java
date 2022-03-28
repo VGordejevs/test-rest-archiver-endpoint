8e4297ed0d5ca746176614302997a77495226325
Vladislavs Gordejevs <vladislavs.gordejevs@gmail.com>
2022-03-28 23:38:13 +0300

    Added base error response model for custom error handling.

---
+++ src/main/java/lv/vladislavs/archiver/exception/ErrorResponse.java
@@ -0,0 +1,12 @@
+package lv.vladislavs.archiver.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ErrorResponse {
+    private String errorCode;
+
+    private String message;
+}
